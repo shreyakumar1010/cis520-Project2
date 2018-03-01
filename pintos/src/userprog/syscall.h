5d7bc11eece8a23881b987ad45aba48ee585d068
B Money <36802937+bristolmc@users.noreply.github.com>
2018-03-01 12:51:01 -0600

    updated prototype in syscall.h

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -5,7 +5,7 @@ void syscall_init (void);
 
 bool userAddressValid (void * virtualAddress, struct thread *t);
 void hald (void);
-void exit (int status);
+void exit (int status, struct thread *t);
 pid_t exec (const char * cmd_line);
 int wait (pid_t pid);
   
