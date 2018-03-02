80ed83b8abb6704071693fd616e6a6e275a1d10c
Bristol Miller <bristolnmiller@cougar.cs.ksu.edu>
2018-03-02 11:09:44 -0600

    added skeletons

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -78,3 +78,52 @@ int wait (pid_t pid)
 {
     return (process_wait (pid));
 }
+
+bool create (const char * file, unsigned initial_size)
+{
+
+}
+
+bool remove (const char * file)
+{
+
+}
+
+int open (const char * file) 
+{
+
+}
+
+int filesize (int fd)
+{
+
+}
+
+int read (int fd, void * buffer, unsigned size)
+{
+
+}
+
+int write (int fd, const void * buffer, unsigned size)
+{
+
+}
+
+void seek (int fd, unsigned position)
+{
+
+}
+
+unsigned tell (int fd)
+{
+
+}
+
+void close (int fd)
+{
+
+}
+
+
+
+
