0bf36c3a6a65c1299277a41b202b425aab47a0e4
Bristol Miller <bristolnmiller@cougar.cs.ksu.edu>
2018-03-02 11:13:00 -0600

    added prototypes to syscall.h

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -8,5 +8,17 @@ void hald (void);
 void exit (int status, struct thread *t);
 pid_t exec (const char * cmd_line);
 int wait (pid_t pid);
-  
+
+bool create(cosnt char * file, unsigned initial_size);
+bool remove(const char * file);
+int open(const char * file);
+int filesize(int fd);
+int read(int fd, void * buffer, unsigned size);
+int write(int fd, const void * buffer, unsigned size);
+void seek(int fd, unsigned position);
+unsigned tell(int fd);
+void close(int fd);
+
+
+
 #endif /* userprog/syscall.h */
