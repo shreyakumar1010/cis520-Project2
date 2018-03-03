f55c6653e9a431354eacd44fa479f48fa0c0be16
coreyvessar <coreyvessar@ksu.edu>
2018-03-03 14:17:52 -0600

    Update syscall.h

--- pintos/src/userprog/syscall.h
+++ pintos/src/userprog/syscall.h
@@ -6,22 +6,22 @@
 void syscall_init (void);
 
 bool userAddressValid (void * virtualAddress, struct thread *t);
-void halt (void);
-void sysexit (int status, struct thread *t); //renamed from exit to sysexit
-tid_t exec (const char * cmd_line);
-int wait (tid_t tid, struct thread * t);
+void sys_halt (void);
+void sys_exit (int status, struct thread *t); //renamed from exit to sysexit
+tid_t sys_exec (const char * cmd_line);
+int sys_wait (tid_t tid, struct thread * t);
 //static int get_user(const uint8_t *uaddr);
 //static bool put_user(uint8_t *udst, uint8_t byte);
 
-bool create(const char * file, unsigned initial_size);
-bool remove(const char * file);
-int open(const char * file);
-int filesize(int fd);
-int read(int fd, void * buffer, unsigned size);
-int write(int fd, const void * buffer, unsigned size);
-void seek(int fd, unsigned position);
-unsigned tell(int fd);
-void close(int fd);
+bool sys_create(const char * file, unsigned initial_size);
+bool sys_remove(const char * file);
+int sys_open(const char * file);
+int sys_filesize(int fd);
+int sys_read(int fd, void * buffer, unsigned size);
+int sys_write(int fd, const void * buffer, unsigned size);
+void sys_seek(int fd, unsigned position);
+unsigned sys_tell(int fd);
+void sys_close(int fd);
 
 /* A struct to keep file descriptor -> file pointer mapping FIX THIS SHIT*/ 
 struct file_desc
