2e47258d25e4b8fd7b44d3a88e5e7f7b476f3026
B Money <36802937+bristolmc@users.noreply.github.com>
2018-03-06 13:03:52 -0600

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -520,12 +520,14 @@ setup_stack (void **esp, int numOfArguments, char *NameThenArguments[20])
 {
   uint8_t *kpage;
   bool success = false;
+  int test = 0;
   kpage = palloc_get_page (PAL_USER | PAL_ZERO);
   if (kpage != NULL) 
   {
       success = install_page (((uint8_t *) PHYS_BASE) - PGSIZE, kpage, true);
       if (success)
       {
+	 ASSERT(test == 1);     
          //Offsetting phys_base as instructed in project 2
          *esp = PHYS_BASE - 12;
          uint32_t * argument_value_pointers_array[numOfArguments];
