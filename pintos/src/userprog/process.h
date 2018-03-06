8344f9bdaa4fd8d2c7c79ac31afeea60a541ed66
coreyvessar <coreyvessar@ksu.edu>
2018-03-06 12:57:19 -0600

    Update process.h

--- pintos/src/userprog/process.h
+++ pintos/src/userprog/process.h
@@ -7,5 +7,7 @@ int process_wait (tid_t, struct thread * t);
 void process_exit (void);
 void process_activate (void);
 
+struct child * get_child(tid_t tid, struct thread *t);
+
 
 #endif /* userprog/process.h */
