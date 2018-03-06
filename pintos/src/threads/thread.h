bd9edf42a6ad6d4035575b35a72c7e8092b9f6f2
coreyvessar <coreyvessar@ksu.edu>
2018-03-06 11:22:41 -0600

    Update thread.h

--- pintos/src/threads/thread.h
+++ pintos/src/threads/thread.h
@@ -119,6 +119,7 @@ struct thread
    struct list children;
    struct semaphore child_semaphore;
    tid_t kid_being_waited_on;
+   struct semaphore prod_sema;
   
    struct list file_list;
    int fd_count; //keeps track of how many files are open
