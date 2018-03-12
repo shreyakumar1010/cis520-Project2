a7612d78d8e7a92ba9c9f46ee8ef9b998ad1cb0c
B Money <36802937+bristolmc@users.noreply.github.com>
2018-03-12 11:33:04 -0500

    Update process.c

--- pintos/src/userprog/process.c
+++ pintos/src/userprog/process.c
@@ -40,6 +40,7 @@ process_execute (const char *file_name_plus_arguments)
   /* Make a copy of FILE_NAME.
      Otherwise there's a race between the caller and load(). */
   fn_copy = palloc_get_page (0);
+	ASSERT(false);
   if (fn_copy == NULL)
   {
 	  ASSERT(false);
